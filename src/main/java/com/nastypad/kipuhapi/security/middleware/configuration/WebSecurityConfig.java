757a58a62975bb9c36c3b58d87bde7eb89e23b5e
LeoGrau <leonardo.grau@outlook.com.pe>
2023-03-23 20:54:16 -0500

    error

--- src/main/java/com/nastypad/kipuhapi/security/middleware/configuration/WebSecurityConfig.java
+++ src/main/java/com/nastypad/kipuhapi/security/middleware/configuration/WebSecurityConfig.java
@@ -4,16 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@Configuration("webSecurityConfiguration")
-public class WebSecurityConfig extends WebSecurityConfiguration {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+@Configuration("wuatThisGotNoName")
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 }
